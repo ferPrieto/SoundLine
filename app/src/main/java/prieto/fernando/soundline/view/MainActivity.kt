b1160153657ae713903d17bf0352970510da0890
Fernando Prieto Moyano <fernando.moyano@nice.agency>
2019-09-09 08:32:15 +0100

    First commit :)

---
+++ app/src/main/java/prieto/fernando/soundline/view/MainActivity.kt
@@ -0,0 +1,16 @@
+package prieto.fernando.soundline.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.main_activity.sound_line_view as soundLineView
+import prieto.fernando.soundline.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        soundLineView.initWaves()
+    }
+}
