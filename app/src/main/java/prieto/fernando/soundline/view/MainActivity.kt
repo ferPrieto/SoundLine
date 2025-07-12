882d76df6e966251698f2ad9442366b09ef5c096
ferPrieto <fprieto@godaddy.com>
2025-07-12 14:43:42 +0100

    Updated gradle and basic config in the project

--- app/src/main/java/prieto/fernando/soundline/view/MainActivity.kt
+++ app/src/main/java/prieto/fernando/soundline/view/MainActivity.kt
@@ -2,14 +2,16 @@ package prieto.fernando.soundline.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.main_activity.sound_line_view as soundLineView
 import prieto.fernando.soundline.R
+import prieto.fernando.soundline.SoundLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        val soundLineView = findViewById<SoundLineView>(R.id.sound_line_view)
         soundLineView.initWaves()
     }
 }
