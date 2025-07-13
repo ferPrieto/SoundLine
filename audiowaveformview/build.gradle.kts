d9a7782ec6a8da46637351aabe28f2fdaea994b1
ferPrieto <fprieto@godaddy.com>
2025-07-13 15:40:01 +0100

    Added palantir plugin for release version helper

--- audiowaveformview/build.gradle.kts
+++ audiowaveformview/build.gradle.kts
@@ -17,30 +17,33 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
-    
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    
+
     kotlinOptions {
         jvmTarget = "17"
     }
-    
+
     buildFeatures {
         compose = true
     }
-    
+
     composeOptions {
         kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
     }
-    
+
     namespace = "com.github.fprieto.audiowaveformview"
     
-    publishing {
+     publishing {
         singleVariant("release") {
             withSourcesJar()
             withJavadocJar()
@@ -51,12 +54,12 @@ android {
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(libs.kotlin.stdlib)
-    
+
     // AndroidX Core Dependencies  
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.core)
-    
+
     // Compose BOM for version alignment
     implementation(platform(libs.compose.bom))
     implementation(libs.compose.ui)
@@ -64,7 +67,7 @@ dependencies {
     implementation(libs.compose.foundation)
     implementation(libs.compose.material3)
     implementation(libs.compose.activity)
-    
+
     // Compose Debug Tools
     debugImplementation(libs.compose.ui.tooling)
     debugImplementation(libs.compose.ui.test.manifest)
@@ -80,28 +83,30 @@ configurations.configureEach {
     }
 }
 
+val gitVersion: groovy.lang.Closure<String> by rootProject.extra
+
 afterEvaluate {
     publishing {
         publications {
             create<MavenPublication>("maven") {
                 from(components["release"])
-                
+
                 groupId = "com.github.fprieto"
                 artifactId = "audiowaveformview"
-                version = "1.0.0"
-                
+                version = gitVersion().removePrefix("v").removeSuffix(".dirty")
+
                 pom {
                     name.set("AudioWaveformView")
                     description.set("A custom Android view that displays audio waveforms with scrollable timeline interface similar to SoundCloud")
                     url.set("https://github.com/fprieto/SoundLine")
-                    
+
                     licenses {
                         license {
                             name.set("The Apache License, Version 2.0")
                             url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
                         }
                     }
-                    
+
                     developers {
                         developer {
                             id.set("fprieto")
@@ -109,7 +114,7 @@ afterEvaluate {
                             email.set("f.prieto.moyano@gmail.com")
                         }
                     }
-                    
+
                     scm {
                         connection.set("scm:git:github.com/fprieto/SoundLine.git")
                         developerConnection.set("scm:git:ssh://github.com/fprieto/SoundLine.git")
@@ -118,16 +123,17 @@ afterEvaluate {
                 }
             }
         }
-        
+
         repositories {
             maven {
                 name = "GitHubPackages"
                 url = uri("https://maven.pkg.github.com/fprieto/SoundLine")
                 credentials {
-                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                    username =
+                        project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                     password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
                 }
             }
         }
     }
-} 
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.git.version)
 }
 
 tasks.register<Delete>("clean") {
